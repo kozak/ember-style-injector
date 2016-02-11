5bfa3cb5f3fd97cb31c5c037b720a5367c70aae1
Michal Kozakiewicz <morswin@gmail.com>
2016-02-11 19:47:54 +0100

    Include js files

--- index.js
+++ index.js
@@ -5,10 +5,16 @@ module.exports = {
   name: 'ember-style-injector',
   contentFor: function (type, config) {
     var links = config.linksToInject;
+    var js = config.jsToInject;
+
     if (type === 'head-footer' && links && links.length) {
       var content = [];
       links.forEach(function(link) {
         content.push('<link rel="stylesheet" href="' + link + '">');
+      });
+
+      js.forEach(function (s) {
+        content.push("<script type='text/javascript' href='" + s +"'> </script>";
       });
       return content.join('\n');
     }
