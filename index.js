4416c8f044b3d2a442749faffcf58f5683392ff9
Michal Kozakiewicz <morswin@gmail.com>
2016-02-11 19:37:10 +0100

    Make it work

--- index.js
+++ index.js
@@ -2,5 +2,15 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-style-injector'
+  name: 'ember-style-injector',
+  contentFor: function (type, config) {
+    var links = config.linksToInject;
+    if (type === 'head-footer' && links && links.length) {
+      var content = [];
+      links.forEach(function(link) {
+        content.push('<link rel="stylesheet" href="' + link + '">');
+      });
+      return content.join('\n');
+    }
+  }
 };
